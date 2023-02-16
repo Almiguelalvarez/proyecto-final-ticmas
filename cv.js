dd5a0114c18c8a45c64427f46371619bf1e2f4b8
Alcides Alvarez <alvarezalcidesmiguel@gmail.com>
2023-02-15 22:58:19 -0300

    ininitial commit

---
+++ cv.js
@@ -0,0 +1,28 @@
+
+document.addEventListener('click', function() {
+    console.log("Por favor, ingrese sus datos y me estare contactando con Ud.");
+    document.getElementById("contacto").innerHTML = "Por favor, ingrese sus datos y me estare contactando con Ud.";
+    });
+    
+    
+    window.onload = function() {
+      window.onload = function() {
+        document.getElementById("boton2").onclick;
+      }
+    }
+    
+    function validar() {
+      var elemento = document.getElementById("telefono").value
+    if (elemento == ""){
+      alert("Debes completar Nombre y Telefono")
+      return false
+     }else {
+      alert("Gracias! Me comunico a la brevedad")
+      return false
+    }
+    }
+    
+    
+    
+    
+    
